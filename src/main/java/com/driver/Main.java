3443ffcd3b6423f9569082a71c2b0bfc3ae9d909
TarunBoyina <tarunboyina33@gmail.com>
2023-03-29 16:54:33 +0530

    Assignment 2 Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[]args){
+      RWOnly obj = new RWOnly();
+
+      /*
+.\Main.java:5: error: cannot find symbol
+        RWOnly obj=new RWOnly();
+                       ^
+  symbol:   class RWOnly
+  location: class Main
+ */
+
+      obj.setName("Tarun");
+      System.out.println(obj.getName());
+  }
 }
